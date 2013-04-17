df96d9ff4431a3147d9b7b32690a8411456eac98
Kristy Miller <miller.kristy06@gmail.com>
2013-04-17 00:32:22 -0400

    Subquery library added, and user profile information pulled correctly from database.

--- 2013-04_MDD-2sided/application/models/userModel.php
+++ 2013-04_MDD-2sided/application/models/userModel.php
@@ -9,30 +9,38 @@ class UserModel extends CI_Model {
     // getDecks
     public function getProfile($userID){
     	
-    	// $this->db->select('COUNT(deck_id) as deck_count, u.user_id');
-    	// $this->db->from('users as u');
-    	// $this->db->join('decks as d', 'u.user_id = d.user_id');
-    	// $this->db->where('u.user_id', $userID);
+    	$this->load->library('subquery');
 
-    	// $this->db->select('card_id, u.user_id, d.deck_id');
-    	// $this->db->from('users');
-    	// $this->db->join('decks as d', 'u.user_id = d.user_id');
-    	// $this->db->where('u.user_id', $userID);
+    	$this->db->select("u.username, u.date_of_reg, u.profile_img");
+	 
+	   	$subDecks = $this->subquery->start_subquery("select");
+		$subDecks->select('COUNT(d.deck_id) as deck_count')->from('users as u');
+		$subDecks->join('decks as d', 'u.user_id = d.user_id');
+		$subDecks->where("u.user_id = '$userID'");
+		$this->subquery->end_subquery('deckCount');
 
-    	// $query = $this->db->query("SELECT username
-		   //  					   FROM users
-		   //  					   WHERE user_id = 5168c94dd332e");
-    	
+    	$subCards = $this->subquery->start_subquery("select");
+    	$subCards->select('COUNT(c.card_id) as card_count')->from('users as u');
+    	$subCards->join('decks as d', 'u.user_id = d.user_id');
+    	$subCards->join('cards as c', 'd.deck_id = c.deck_id');
+		$subCards->where("u.user_id = '$userID'");
+    	$this->subquery->end_subquery('cardCount');
 
-    	// $query = $this->db->get();
+    	$subTags = $this->subquery->start_subquery("select");
+    	$subTags->select('COUNT(t.tag_id) as tag_count')->from('users as u');
+    	$subTags->join('decks as d', 'u.user_id = d.user_id');
+    	$subTags->join('tags as t', 'd.deck_id = t.deck_id');
+		$subTags->where("u.user_id = '$userID'");
+    	$this->subquery->end_subquery('tagsCount');
 
-    	$this->db->select("*");
-		$this->db->from(
-			$this->db->select('c.card_id, u.user_id');
-			$this->db->from('users');
-			$this->db->join('decks as d', 'u.user_id = d.user_id');
-			$this->db->join('cards as c', 'd.deck_id = c.deck_id');
-		);
+    	$subFriends = $this->subquery->start_subquery("select");
+    	$subFriends->select('COUNT(uf.friend_id) as friend_count')->from('users as u');
+    	$subFriends->join('user_friends as uf', 'u.user_id = uf.user_id');
+		$subFriends->where("u.user_id = '$userID'");
+    	$this->subquery->end_subquery('friendsCount');
+
+    	$this->db->from('users as u');
+    	$this->db->where("u.user_id = '$userID'");
 
 		$query = $this->db->get();
 
@@ -41,19 +49,14 @@ class UserModel extends CI_Model {
     		foreach($query->result() as $row){
     			$dataResults[] = $row;
     		}
-
-	    	echo "<pre>";
-	    	var_dump($dataResults);
-	    	echo "</pre>";
-
     	}else{
     		// No Results Found
     		echo "No Results";
     	}
 
-    	// echo '<pre>';
-    	// print_r($dataResults);
-    	// echo '</pre>';
+    	echo '<pre>';
+    	print_r($dataResults);
+    	echo '</pre>';
 
     	// if($dataResults){
     	// 	return $dataResults;
--- 2013-04_MDD-2sided/system/database/DB_active_rec.php
+++ 2013-04_MDD-2sided/system/database/DB_active_rec.php
@@ -1663,6 +1663,35 @@ class CI_DB_active_record extends CI_DB_driver {
 	// --------------------------------------------------------------------
 
 	/**
+	 * Get SELECT query string
+	 *
+	 * Compiles a SELECT query string and returns the sql.
+	 *
+	 * @param    string    the table name to select from (optional)
+	 * @param    bool    TRUE: resets QB values; FALSE: leave QB vaules alone
+	 * @return    string
+	 */
+	public function get_compiled_select($table = '', $reset = TRUE)
+	{
+	    if ($table !== '')
+	    {
+	        $this->_track_aliases($table);
+	        $this->from($table);
+	    }
+
+	    $select = $this->_compile_select();
+
+	    if ($reset === TRUE)
+	    {
+	        $this->_reset_select();
+	    }
+
+	    return $select;
+	}
+
+	// --------------------------------------------------------------------
+
+	/**
 	 * Compile the SELECT statement
 	 *
 	 * Generates a query string based on which functions were used.
