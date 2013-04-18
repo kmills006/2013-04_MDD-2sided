228d5c533381809d2dff8a1d887094670323e0f3
kolbysisk <kolby.sisk@gmail.com>
2013-04-18 11:29:32 -0400

    finish html and css for profiles

--- 2013-04_MDD-2sided/application/views/userProfile.php
+++ 2013-04_MDD-2sided/application/views/userProfile.php
@@ -21,18 +21,18 @@
 				<h3>343 Check Marks!</h3>
 			</article>
 		</section>
-		<section>
+		<section id="quickInfo">
 			<ul>
-				<li>23<span>Friends</span></li>
-				<li>11<span>Decks</span></li>
-				<li>45<span>Cards</span></li>
-				<li>101<span>Comments</span></li>
-				<li>33<span>Tags</span></li>
+				<li><h1>23</h1><h2>Friends</h2></li>
+				<li><h1>11</h1><h2>Decks</h2></li>
+				<li><h1>45</h1><h2>Cards</h2></li>
+				<li><h1>101</h1><h2>Comments</h2></li>
+				<li class="last"><h1>33</h1><h2>Tags</h2></li>
 			</ul>
 		</section>
-		<section>
+		<section id="bgroup">
 			<button>Edit Profile</button>
-			<button>Notifications(11)</button>
+			<button>Notifications<span>(11)</span></button>
 		</section>
 	</div>	
 </div> 
@@ -40,13 +40,20 @@
 	<div class="dbbg"><div class="sizer"><h1>Activity</h1></div></div>
 	<div class="sizer">
 		<ul>
-			<li>New Deck! Kristy123 created a new deck called Spelling ABC.</li>
-			<li>New Friend! Kristy123 and Kolby99 are now friends.</li>
-			<li>New Badge! Kristy123 recieved the badge Deck Master.</li>
-			<li>New Deck! Kristy123 created a new deck called Spelling ABC. </li>
-			<li>New Friend! Kristy123 and Kolby99 are now friends.</li>
-			<li>New Deck! Kristy123 created a new deck called Spelling ABC.</li>
-			<li>New Badge! Kristy123 recieved the badge Deck Master.</li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Deck! Kristy123 created a new deck called Spelling ABC.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Friend! Kristy123 and Kolby99 are now friends.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Badge! Kristy123 recieved the badge Deck Master.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Deck! Kristy123 created a new deck called Spelling ABC.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Friend! Kristy123 and Kolby99 are now friends.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Deck! Kristy123 created a new deck called Spelling ABC.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Badge! Kristy123 recieved the badge Deck Master.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Deck! Kristy123 created a new deck called Spelling ABC.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Friend! Kristy123 and Kolby99 are now friends.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Badge! Kristy123 recieved the badge Deck Master.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Deck! Kristy123 created a new deck called Spelling ABC.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Friend! Kristy123 and Kolby99 are now friends.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Deck! Kristy123 created a new deck called Spelling ABC.</p></div></li>
+			<li><div class="sizer"><img src="" alt="" width="50" height="50"><p>New Badge! Kristy123 recieved the badge Deck Master.</p></div></li>
 		</ul>
 	</div>
 </div>
