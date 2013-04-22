9107e7bed28eefdea73d2892cdd1b86d91e0e220
kolbysisk <kolby.sisk@gmail.com>
2013-04-22 02:06:50 -0400

    added html, css, and javascript for cards page

---
+++ 2013-04_MDD-2sided/application/views/viewCards.php
@@ -0,0 +1,69 @@
+    <div id="content">    
+        <section id="cards">
+            <ul>
+               <li class="aCard">
+                    <h1 class="question">0+1</h1>
+                    <h1 class="answer">1</h1>
+                </li>
+               <li class="aCard">
+                    <h1 class="question">0+1</h1>
+                    <h1 class="answer">1</h1>
+                </li>
+               <li class="aCard">
+                    <h1 class="question">0+1</h1>
+                    <h1 class="answer">1</h1>
+                </li>
+               <li class="aCard">
+                    <h1 class="question">0+1</h1>
+                    <h1 class="answer">1</h1>
+                </li>
+               <li class="aCard">
+                    <h1 class="question">0+1</h1>
+                    <h1 class="answer">1</h1>
+                </li>
+               <li class="aCard">
+                    <h1 class="question">0+1</h1>
+                    <h1 class="answer">1</h1>
+                </li>
+            </ul>
+        </section>
+        	
+		<section id="cardT">
+			<button type="button" id="editButton" style="visibility:hidden;">Edit</button>
+			<button type="button" id="deleteButton" style="visibility:hidden;">Delete</button>
+			<div id="cardTools">
+				<div id="toolButtons">
+					<div id="leftArrow"></div>
+					<button type="button" id="lilFLip">Flip Card</button>
+					<div id="rightArrow"></div>
+				</div>
+			</div>
+			<button type="button" id="randomButton">Random</button>
+		</section>
+
+		<p id="progNum"></p>
+		<div id="prog">
+			<div id="p"></div>
+		</div>
+
+		<section id="deckTags">
+			<div class="sizer">
+				<ul class="yourTags">
+			
+				</ul>			
+			</div>
+	</div> <!-- end of content -->
+
+    <!-- Jquery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<? echo base_url(); ?>js/libs/jquery-1.9.1.min.js"><\/script>')</script>
+    
+    <!-- Plugins -->
+    <script type="text/javascript" src="<? echo base_url(); ?>js/plugins/rotate3Di.js"></script>
+
+    <!-- Scripts -->
+    <script type="text/javascript" src="<? echo base_url(); ?>js/main.js"></script>
+
+    <!-- Inits -->
+    <script type="text/javascript">initCard();</script>
+
--- 2013-04_MDD-2sided/js/main.js
+++ 2013-04_MDD-2sided/js/main.js
@@ -50,6 +50,7 @@ var initValidation = function(){
 };
 
 var initDeck = function(){
+	console.log('few');
 	//Opens and closes deck's settings
 	var options = $('.options');
 
@@ -61,6 +62,145 @@ var initDeck = function(){
 			options.removeClass('open');
 			opened ? that.removeClass('open') : that.addClass('open');
 		}
-	}); 
+	});
+};
+
+
+
+
+
+
+var initCard = function(){
+	//Functionality for cards page including cycling through cards.
+	var shuffle = [];
+	var shuffled = [];
+
+	var initShuffle = function(){
+		for (var i = 0; i < $('.aCard').length; i++) {
+			shuffle.push(i);
+		}
+	};
+
+	var initCards = function(ind){
+
+		if($(".activeCardBack")[0]){
+			$('.activeCardBack').rotate3Di('unflip', 180, {direction: 'clockwise', sideChange: flipBack});
+		}else{
+			var cards = $('.aCard');
+			$('.activeCard').removeClass('activeCard');
+			$('.aCard').addClass('hideCard');
+
+			if(ind !== 0)cards.eq(ind-1).addClass('leftCard').removeClass('hideCard rightCard activeCard');
+			cards.eq(ind).addClass('activeCard').removeClass('hideCard rightCard leftCard');
+			cards.eq(ind+1).addClass('rightCard').removeClass('hideCard leftCard activeCard');
+
+			var top = $('.activeCard').index() + 1;
+			var bot = $('.aCard').length;
+			var per = top/bot * 100 + '%';
+
+			$('#progNum').html(top + '/' + bot);
+
+			$('#p').animate({
+				width: per
+			}, 1000, function() {
+				if(per == '100%'){
+					$('#p').addClass('pFull');
+				}else{
+					$('.pFull').removeClass('pFull');
+				}
+			});
+		}
+	};
+
+	//ON CLICK FUNCTIONS FOR CARD TOOLS
+	$('#leftArrow').on('click', function(e){
+		if($('.activeCard').index() !== 0 && $('.cardedit')[0] === undefined)initCards($('.activeCard').index()-1);
+	});
+	$('#rightArrow').on('click', function(e){
+		if($('.activeCard').index() != $('.aCard').length-1 && $('.cardedit')[0] === undefined)initCards($('.activeCard').index()+1);
+	});
+	$('#randomButton').on('click', function(e){
+		if($('.cardedit')[0] === undefined){
+			var ind = shuffle[Math.floor(Math.random()*shuffle.length)];
+			shuffle.splice(shuffle.indexOf(ind),1);
+			shuffled.push(ind);
+			initCards(ind);
+
+			if(shuffle.length === 0){
+				$.each(shuffled, function(key, value) {
+					shuffle.push(value);
+					shuffled = [];
+				});
+			}
+		}
+	});
+
+	//ON KEYDOWN FUNCTIONS FOR CARD TOOLS
+	$(document).keydown(function(e){
+		if(e.keyCode == 32 && $('#question')[0] === undefined && $('#answer')[0] === undefined && $('#cards')[0] && $('.cardedit')[0] === undefined && $(document.activeElement).attr('id') != 'searchIni' && $(document.activeElement).attr('class') != 'res'){
+			$('.activeCard').rotate3Di('flip', 180, {direction: 'clockwise', sideChange: flipCard});
+			$('.activeCardBack').rotate3Di('unflip', 180, {direction: 'clockwise', sideChange: flipBack});
+			return false;
+		}else if(e.keyCode == 37 && $('#question')[0] === undefined && $('#answer')[0] === undefined && $('#cards')[0] && $('.cardedit')[0] === undefined && $(document.activeElement).attr('id') != 'searchIni' && $(document.activeElement).attr('class') != 'res'){
+			if($('.activeCard').index() !== 0)initCards($('.activeCard').index()-1);
+			return false;
+		}else if(e.which == 39 && $('#question')[0] === undefined && $('#answer')[0] === undefined && $('#cards')[0] && $('.cardedit')[0] === undefined && $(document.activeElement).attr('id') != 'searchIni' && $(document.activeElement).attr('class') != 'res'){
+			if($('.activeCard').index() != $('.aCard').length-1)initCards($('.activeCard').index()+1);
+			return false;
+		}else if(e.which == 40 && $('#question')[0] === undefined && $('#answer')[0] === undefined && $('#cards')[0] && $('.cardedit')[0] === undefined && $(document.activeElement).attr('id') != 'searchIni' && $(document.activeElement).attr('class') != 'res'){
+			var ind = shuffle[Math.floor(Math.random()*shuffle.length)];
+			shuffle.splice(shuffle.indexOf(ind),1);
+			shuffled.push(ind);
+			initCards(ind);
+			if(shuffle.length === 0){
+				$.each(shuffled, function(key, value) {
+					shuffle.push(value);
+					shuffled = [];
+				});
+			}
+			return false;
+		}
+	});
+	//SCRIPTS FOR FLIPPING CARD//
+	var currentCardTitle;
+	var currentCardAnswer;
+
+	var flipCard = function() {
+		var that = $(this);
+		currentCardTitle = that.find('.question').text();
+        currentCardAnswer = that.find('.answer').text();
+		that.removeClass('activeCard');
+        that.addClass('activeCardBack');
+        that.find('.question').html(currentCardAnswer);
+    };
+    var flipCardQ = function(){
+		var that = $(this);
+		currentCardTitle = that.find('.question').text();
+        currentCardAnswer = that.find('.answer').text();
+		that.removeClass('activeCard');
+        that.addClass('activeCardBack');
+		$('.activeCardBack .question').replaceWith('<textarea id="answer" type="text" name="canswer" placeholder="Enter your answer here then press enter"></textarea>');
+		$('#answer').focus();
+
+    };
+    var flipBackQ = function(){
+		var that = $(this);
+		ans = that.find('#answer').val();
+		that.find('#answer').parent().replaceWith('<li class="aCard activeCard"><h1 class="question">' + currentCardTitle + '</h1><h1 class="answer">'+ ans +'</h1></li>');
+    };
+    var flipBack = function() {
+		var that = $(this);
+        that.removeClass('activeCardBack');
+        that.addClass('activeCard');
+        that.find('.question').html(currentCardTitle);
+    };
+	$('#lilFLip').on('click', function(e){
+		if($('.cardedit')[0] === undefined){
+			$('.activeCard').rotate3Di('flip', 180, {direction: 'clockwise', sideChange: flipCard});
+			$('.activeCardBack').rotate3Di('unflip', 180, {direction: 'clockwise', sideChange: flipBack});
+		}
+	});
+	initShuffle();
+	initCards(0);
 };
 
