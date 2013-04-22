a2a004b22b46d7aac832bd3a121b2150b317ba51
kolbysisk <kolby.sisk@gmail.com>
2013-04-21 22:09:09 -0400

    login/register validation

--- 2013-04_MDD-2sided/application/views/includes/footer.php
+++ 2013-04_MDD-2sided/application/views/includes/footer.php
@@ -3,16 +3,5 @@
 				<p>Copyright &copy; 2013 Kolby Sisk & Kristy Miller. All rights reserved.</p>
 			</div>
 		</footer>	
-
-
-		<!-- Jquery -->
-		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
-		
-		<!-- Plugins -->
-		<script type="text/javascript" src="<? echo base_url(); ?>js/plugins/rotate3Di.js"></script>
-		
-		<!-- Scripts -->
-		<script type="text/javascript" src="<? echo base_url(); ?>js/main.js"></script>
-		
 	</body>
 </html>
