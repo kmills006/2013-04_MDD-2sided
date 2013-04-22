3c9bef8c666bfc1a4de488ffb57419e7ad6df23b
kolbysisk <kolby.sisk@gmail.com>
2013-04-22 19:57:33 -0400

    media quires added

--- 2013-04_MDD-2sided/application/views/includes/landingHeader.php
+++ 2013-04_MDD-2sided/application/views/includes/landingHeader.php
@@ -9,8 +9,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>2sided - The Social Way to Study</title>
 
-	<!-- Styles -->
-	<link rel="stylesheet" type="text/css" href="<? echo base_url(); ?>css/style.css" media="screen" /> 
+	<!-- Desktop -->
+	<link rel="stylesheet" href="<? echo base_url(); ?>css/style.css" media="only screen and (min-width: 769px)" > 
+	<!-- Tablet -->
+	<link rel="stylesheet" href="<? echo base_url(); ?>css/tablet-style.css" media="only screen and (max-width: 768px)" > 
+	<!-- Mobile -->
+	<link rel="stylesheet" href="<? echo base_url(); ?>css/mobile-style.css" media="only screen and (max-width: 480px)" > 
 	
 	<!-- Favicon -->
 	<?php echo link_tag("favicon.ico", "shortcut icon", "image/ico");?>
--- 2013-04_MDD-2sided/application/views/includes/loggedInHeader.php
+++ 2013-04_MDD-2sided/application/views/includes/loggedInHeader.php
@@ -13,9 +13,11 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>2sided - The Social Way to Study</title>
 
-	<!-- Styles -->
+	<!-- Desktop -->
 	<link rel="stylesheet" href="<? echo base_url(); ?>css/style.css" media="only screen and (min-width: 769px)" > 
+	<!-- Tablet -->
 	<link rel="stylesheet" href="<? echo base_url(); ?>css/tablet-style.css" media="only screen and (max-width: 768px)" > 
+	<!-- Mobile -->
 	<link rel="stylesheet" href="<? echo base_url(); ?>css/mobile-style.css" media="only screen and (max-width: 480px)" > 
 
 	<!-- Favicon -->
