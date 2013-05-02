a3688fa28468600822aa00a7f8f900ce80a15c96
kolbysisk <kolby.sisk@gmail.com>
2013-05-02 02:01:27 -0400

    added footer

--- 2013-04_MDD-2sided/application/views/includes/footer.php
+++ 2013-04_MDD-2sided/application/views/includes/footer.php
@@ -1,9 +1,31 @@
+	<div class="bgPoint"></div>
 	<footer>
-		<div id="sizer">
-<!-- 			<p>Copyright &copy; 2013 Kolby Sisk & Kristy Miller. All rights reserved.</p>
- -->		</div>
+		<div class="sizer">
+			<section id="newsletter">
+				<h1>Newsletter</h1>
+				<p>Sign up for our newsletter to hear about 2sided’s future updates.</p>
+				<input type="text" placeholder="example@domain.com"></input>
+			</section>
+
+			<div class="buttonbg"></div>
+			<button>Add A Deck</button>
+
+			<section id="footerNav">
+				<h1>Site Navigation</h1>
+				<ul>
+					<li>Browse Decks</li>
+					<li>Browse Users</li>
+					<li>About 2sided</li>
+					<li>Sign Up | Log In</li>
+					<li>Support</li>
+					<li>Contact Us</li>
+				</ul>
+			</section>
+		</div>
+		<section id="bottomFooter">
+			<p>Copyright &copy; 2013 Kolby Sisk & Kristy Miller. All rights reserved.</p>
+		</section>
 	</footer>	
-	
 	<script type="text/javascript" src="<? echo base_url(); ?>js/responsive.js"></script>
 </body>
 </html>
